4210221af7762cc5f564aae177931b1178655471
Benjamen Wahle <saxandbears@gmail.com>
2024-12-06 09:11:21 -0600

    minor changes to order confirmation

--- views/orderConfirmation.js
+++ views/orderConfirmation.js
@@ -4,6 +4,9 @@ export default state => html`
   <main>
     <h3 style="text-align:center">Order Summary</h3>
     <!-- <div id="noOrder">[No Order Found]</div> -->
-    <p id="orderReceipt">${JSON.stringify(state.order)}</p>
+    <p id="orderReceipt">
+      ${JSON.stringify(state.order.items)}
+    </p>
+    <div id="orderReceipt2"></div>
   </main>
 `;
